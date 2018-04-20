a7db09f824850091753f5f85ca61198669da3a79
coss8484 <sabotage84@mail.ru>
2018-04-20 16:56:59 +0400

    IMPR: materials view

---
+++ StuffSite2/App_Data/dbo.MaterialsTable.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Name] NCHAR(50) NOT NULL, 
+    [Descriotion] NCHAR(500) NULL, 
+    [Price] NUMERIC NULL, 
+    [IsOnStorage] BIT NOT NULL
+)
--- StuffSite2/Controllers/MaterialsController.cs
+++ StuffSite2/Controllers/MaterialsController.cs
@@ -13,7 +13,7 @@ namespace StuffSite2.Controllers
         public ActionResult Index(int? id)
         {
             List<Product> products= FeelProducts();
-            //ViewBag.var = id;
+            
             Product temp = null;
 
             foreach (var item in products)
@@ -34,8 +34,8 @@ namespace StuffSite2.Controllers
             List<Product> products = new List<Product>();
 
             products.Add(new Product {ID=1, Name = "FIMO", Image = "/images/81.jpg", Description = "Самая крутая глина в мире! Налетай, торопись покупай глину и лепи живопись)", IsInStock = true, Price = 19.99 });
-            products.Add(new Product { ID = 2, Name = "Баба в кокошнике", Image = "~/images/df8010200210l.jpg", Description = "Самая крутая глина в мире! Налетай, торопись покупай глину и лепи живопись)", IsInStock = false, Price = 9.99 });
-            products.Add(new Product { ID = 3, Name = "ГЛИНА ИЗ БЕРЛИНА", Image = "~/images/df8010200190l.jpg", Description = "Самая крутая глина в мире! Налетай, торопись покупай глину и лепи живопись)", IsInStock = true, Price = 29.99 });
+            products.Add(new Product { ID = 2, Name = "Баба в кокошнике", Image = "/images/df8010200210l.jpg", Description = "Самая крутая глина в мире! Налетай, торопись покупай глину и лепи живопись)", IsInStock = false, Price = 9.99 });
+            products.Add(new Product { ID = 3, Name = "ГЛИНА ИЗ БЕРЛИНА", Image = "/images/df8010200190l.jpg", Description = "Самая крутая глина в мире! Налетай, торопись покупай глину и лепи живопись)", IsInStock = true, Price = 29.99 });
             return products;
         }
     }
